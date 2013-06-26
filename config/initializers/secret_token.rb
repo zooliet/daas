2563f985a4a8528af9da8cbab371a52460469474
Junhyun Shin <hl1sqi@gmail.com>
2013-06-26 17:23:09 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daas::Application.config.secret_token = '6a562fc52b8704259c63fecb304deed92a4300fc82b41aee7ec72e3352c2a67fdbbeab67547342fb06098db6c3bc5b7d5c4bb44f5df2396393ba054601e388dc'
